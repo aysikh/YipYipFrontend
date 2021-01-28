bc913042788c683dbfc19929b8b1b8cffbcd9dac
aysikh <amy.sikhammountry@gmail.com>
2021-01-28 15:00:37 -0500

    added logo

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React from 'react'
 import './App.css';
 import NavBar from './containers/NavBar'
 import MainContainer from './containers/MainContainer'
+import Logo from './assets/yipyiplogo2.png'
+import { makeStyles } from '@material-ui/core/styles';
+
 
 function App() {
+  const useStyles = makeStyles({
+    logo: {
+      maxWidth: '12%',
+    }
+  })
+
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
   return (
     <div className="App">
-      <h1> APP . JS</h1> 
+      <center>
+        <img src={Logo} className={classes.logo}/> 
+      </center>
       <NavBar />
       <MainContainer />
     </div>
